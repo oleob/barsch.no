3505466acc8b130072ab692d271c0df3533b2137
oleob <ole.barsch@gmail.com>
2017-10-30 16:45:23 +0100

    Open links in new tab

--- src/components/MediaLink.jsx
+++ src/components/MediaLink.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MediaLink = (props) => (
   <div className="MediaLink">
-    <a href={props.link}>
+    <a href={props.link} target="_blank">
       <img alt="linkicon" className="MediaLinkIcon" src={props.icon} />
     </a>
   </div>
