80e9839d78adb9faf3e190c3c38f1f610614673f
oleob <ole.barsch@gmail.com>
2017-10-31 12:56:11 +0100

    Add circular menu to main page

--- src/containers/Content.jsx
+++ src/containers/Content.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import LinkBar from '../components/LinkBar';
 import Construction from '../components/Construction';
 //import HiddenMenu from '../components/HiddenMenu';
-import Hexagon from '../components/Hexagon';
+// import Hexagon from '../components/Hexagon';
+import CircularMenu from '../components/CircularMenu';
 
 const Content = () => (
   <div className="Content">
     <div className="ContentCenter">
       {/* <HiddenMenu /> */}
-      <Hexagon />
+      {/* <Hexagon /> */}
+      <CircularMenu radius={150} nodeCount={6} />
       <Construction />
       <h1>Under construction!</h1>
       <LinkBar />
