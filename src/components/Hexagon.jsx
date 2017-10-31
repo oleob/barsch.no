bef01538505ffe113033ac6f802a8d3bbe3edd55
oleob <oleob@stud.ntnu.no>
2017-10-31 01:27:21 +0100

    Add hexagon

---
+++ src/components/Hexagon.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Hexagon = () => (
+  <div className="HexContainer">
+    <div className="Hex0" />
+    <div className="Hex1" />
+    <div className="Hex2" />
+    <div className="Hex3" />
+    <div className="Hex4" />
+    <div className="Hex5" />
+    <div className="Hex6" />
+  </div>
+);
+
+export default Hexagon;
