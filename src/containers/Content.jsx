350864e04123ce1ac562220f227994064ab08750
oleob <oleob@stud.ntnu.no>
2017-10-31 23:06:50 +0100

    Moved Content to Home

--- src/containers/Content.jsx
+++ src/containers/Content.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import LinkBar from '../components/LinkBar';
-import Construction from '../components/Construction';
-//import HiddenMenu from '../components/HiddenMenu';
-// import Hexagon from '../components/Hexagon';
-import CircularMenu from '../components/CircularMenu';
+import Routes from './Routes';
 
 const Content = () => (
   <div className="Content">
-    <div className="ContentCenter">
-      {/* <HiddenMenu /> */}
-      {/* <Hexagon /> */}
-      <CircularMenu radius={150} nodeCount={6} />
-      <Construction />
-      <h1>Under construction!</h1>
-      <LinkBar />
-    </div>
+    <Routes />
   </div>
 )
 
