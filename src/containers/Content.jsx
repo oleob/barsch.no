5d64bf9833f65918112dc626368a75ec754100c8
oleob <oleob@stud.ntnu.no>
2017-10-31 01:27:38 +0100

    Add hexagon to page

--- src/containers/Content.jsx
+++ src/containers/Content.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import LinkBar from '../components/LinkBar';
 import Construction from '../components/Construction';
+//import HiddenMenu from '../components/HiddenMenu';
+import Hexagon from '../components/Hexagon';
+
 const Content = () => (
   <div className="Content">
     <div className="ContentCenter">
+      {/* <HiddenMenu /> */}
+      <Hexagon />
       <Construction />
       <h1>Under construction!</h1>
       <LinkBar />
