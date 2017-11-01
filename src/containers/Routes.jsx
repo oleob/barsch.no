79845b82614360f018282204d5589643f38e3d0a
oleob <ole.barsch@gmail.com>
2017-11-01 13:53:19 +0100

    Add projects and 404 to routes

--- src/containers/Routes.jsx
+++ src/containers/Routes.jsx
@@ -3,12 +3,16 @@ import { BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
+import Projects from './pages/Projects';
+import Page404 from './pages/Page404';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/projects" component={Projects} />
+      <Route path="*" component={Page404} />
     </Switch>
   </BrowserRouter>
 );
