942eaf538286a992ac193f1fe5654eeb027cee38
oleob <ole.barsch@gmail.com>
2017-11-01 13:52:49 +0100

    Add notfound component

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Magic from '../icons/magic.svg';
+
+
+const NotFound = () => (
+  <img alt="Magic hat" className="Icon" src = {Magic} />
+);
+
+export default NotFound;
