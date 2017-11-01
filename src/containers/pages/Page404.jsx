4885ebf07fe4ccdb93a3418a55a01a0c7d7872c3
oleob <ole.barsch@gmail.com>
2017-11-01 13:52:37 +0100

    Add 404 page

---
+++ src/containers/pages/Page404.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NotFound from '../../components/NotFound';
+
+
+const Page404 = () => (
+  <div className="ContentCenter">
+    <NotFound />
+    <h1>404</h1>
+    <h1>Page not found</h1>
+  </div>
+);
+
+export default Page404;
