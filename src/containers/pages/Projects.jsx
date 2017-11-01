10d56c7f34245a5a97e054b69ce936bc67242774
oleob <ole.barsch@gmail.com>
2017-11-01 13:52:23 +0100

    Add projects page

---
+++ src/containers/pages/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Construction from '../../components/Construction';
+
+const Projects = () => (
+  <div className="ContentCenter">
+    <Construction />
+    <h1>Under Construction</h1>
+  </div>
+)
+
+export default Projects;
