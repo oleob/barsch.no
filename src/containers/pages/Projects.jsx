3b3e5194421eba82d2db412a98742e9d3140e20b
oleob <oleob@stud.ntnu.no>
2017-11-03 00:04:47 +0100

    Add HomeButton to projectsPage

--- src/containers/pages/Projects.jsx
+++ src/containers/pages/Projects.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import Construction from '../../components/Construction';
+import HomeButton from '../../components/HomeButton';
 
 const Projects = () => (
   <div className="ContentCenter">
+    <HomeButton />
     <Construction />
     <h1>Under Construction</h1>
   </div>
