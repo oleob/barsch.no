e8edf5a2c77efd5888246c2f25e3d76b0e6dde3b
oleob <oleob@stud.ntnu.no>
2017-11-03 00:04:58 +0100

    Add HomeButton to aboutPage

--- src/containers/pages/About.jsx
+++ src/containers/pages/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import Construction from '../../components/Construction';
+import HomeButton from '../../components/HomeButton';
 
 const About = () => (
   <div className="ContentCenter">
+    <HomeButton />
     <Construction />
     <h1>Under Construction</h1>
   </div>
