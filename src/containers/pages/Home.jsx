6e4f0633c6253a0ef7897cc66b205b70ed36a869
oleob <oleob@stud.ntnu.no>
2018-06-03 01:32:09 +0200

    Add Link to battery page

--- src/containers/pages/Home.jsx
+++ src/containers/pages/Home.jsx
@@ -10,6 +10,7 @@ import Linkedin from '../../icons/linkedin.svg';
 import Facebook from '../../icons/facebook.svg';
 import Info from '../../icons/info.svg';
 import Code from '../../icons/code.svg';
+import Battery from '../../icons/battery.svg';
 
 const Home = () => (
   <div className="ContentCenter">
@@ -18,6 +19,7 @@ const Home = () => (
       <MenuNode link="https://github.com/oleob/" icon={Github} />
       <MenuNode link="https://www.linkedin.com/in/ole-barsch/" icon={Linkedin} />
       <MenuNode link="https://www.facebook.com/olebarsch" icon={Facebook} />
+      <LocalMenuNode link="/battery" icon={Battery} />
       <LocalMenuNode link="/about" icon={Info} />
       <LocalMenuNode link="/projects" icon={Code} />
     </CircularMenu>
