82c45ef9abbe08a69c872e696a45925056b199f7
oleob <oleob@stud.ntnu.no>
2018-06-03 01:32:17 +0200

    Update about page

--- src/containers/pages/About.jsx
+++ src/containers/pages/About.jsx
@@ -8,6 +8,9 @@ const About = () => (
     <HomeButton />
     <Construction />
     <h1>Under Construction</h1>
+    <h2>All Icons on this page are downloaded from Iconmnstr
+      <a href="https://iconmonstr.com/">(link here)</a>
+    </h2>
   </div>
 )
 
